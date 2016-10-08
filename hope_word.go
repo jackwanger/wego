3ea3cb0d660f81a6a5d7c6c9141f4ee12d936b89
goofansu <goofan.su@gmail.com>
2016-10-08 19:43:13 +0800

    create temp dict

--- hope_word.go
+++ hope_word.go
@@ -15,36 +15,26 @@ import (
 	"github.com/huichen/sego"
 )
 
-var segmenter sego.Segmenter
 var port int
+var segmenter sego.Segmenter
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	flag.IntVar(&port, "port", 8000, "listen port")
 	flag.Parse()
+}
 
-	if err := prepareDict(); err != nil {
+func main() {
+	files, err := prepareDict()
+	if err != nil {
 		log.Fatalf("%v", err)
 		os.Exit(1)
 	}
 
-	dict := strings.Join(AssetNames(), ",")
+	defer removeDict(files)
+	dict := strings.Join(files, ",")
 	segmenter.LoadDictionary(dict)
-}
 
-//go:generate go-bindata -prefix "dict/" -pkg main -o dict.go dict/
-func prepareDict() error {
-	for _, v := range AssetNames() {
-		data, _ := Asset(v)
-		err := ioutil.WriteFile(v, []byte(data), 0644)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func main() {
 	r := gin.Default()
 	r.POST("/validate", validateEndPoint)
 	r.POST("/filter", filterEndPoint)
@@ -54,14 +44,14 @@ func main() {
 func validateEndPoint(c *gin.Context) {
 	text := c.PostForm("message")
 	segments := segmenter.Segment([]byte(text))
-	if IsContainInvalidWord(segments) {
+	if existInvalidWord(segments) {
 		c.JSON(200, gin.H{"result": "false"})
 	} else {
 		c.JSON(200, gin.H{"result": "true"})
 	}
 }
 
-func IsContainInvalidWord(segments []sego.Segment) bool {
+func existInvalidWord(segments []sego.Segment) bool {
 	for _, seg := range segments {
 		token := seg.Token()
 		if token.Frequency() > 1 {
@@ -90,4 +80,41 @@ func replaceInvalidWords(segments []sego.Segment, text string) string {
 		}
 	}
 	return text
+}
+
+//go:generate go-bindata -prefix "dict/" -pkg main -o dict.go dict/
+func prepareDict() ([]string, error) {
+	var files = make([]string, 0)
+	for _, v := range AssetNames() {
+		data, err := Asset(v)
+		if err != nil {
+			return nil, err
+		}
+		f, err := saveTempFile(v, data)
+		if err != nil {
+			return nil, err
+		}
+		files = append(files, f.Name())
+	}
+	return files, nil
+}
+
+func saveTempFile(asset string, data []byte) (*os.File, error) {
+	tmpfile, err := ioutil.TempFile("", asset)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := tmpfile.Write(data); err != nil {
+		return nil, err
+	}
+	if err := tmpfile.Close(); err != nil {
+		return nil, err
+	}
+	return tmpfile, nil
+}
+
+func removeDict(files []string) {
+	for _, f := range files {
+		os.Remove(f)
+	}
 }
